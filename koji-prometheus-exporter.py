cbf6d3f607f2b69c59da980a0b9aa17bc1b92a9a
Ralph Bean <rbean@redhat.com>
2020-09-12 09:16:47 -0400

    Report on errors and in progress tasks too

--- koji-prometheus-exporter.py
+++ koji-prometheus-exporter.py
@@ -10,7 +10,7 @@ import time
 
 import koji
 
-from prometheus_client.core import REGISTRY, CounterMetricFamily
+from prometheus_client.core import REGISTRY, CounterMetricFamily, GaugeMetricFamily
 from prometheus_client import start_http_server
 
 
@@ -24,10 +24,27 @@ START = time.time()
 metrics = {}
 
 
-def koji_tasks_total():
+error_states = [koji.TASK_STATES['FAILED']]
+in_progress_states = [
+    koji.TASK_STATES['FREE'],
+    koji.TASK_STATES['OPEN'],
+    koji.TASK_STATES['ASSIGNED'],
+]
+
+
+def retrieve_recent_koji_tasks():
     b = koji.ClientSession(KOJI_URL)
     tasks = b.listTasks(opts=dict(createdAfter=START))
+    return tasks
 
+
+def retrieve_open_koji_tasks():
+    b = koji.ClientSession(KOJI_URL)
+    tasks = b.listTasks(opts=dict(state=in_progress_states))
+    return tasks
+
+
+def koji_tasks_total(tasks):
     counts = {}
     for task in tasks:
         method = task['method']
@@ -41,12 +58,42 @@ def koji_tasks_total():
             yield counts[channel][method], [channel, method]
 
 
+def only(tasks, states):
+    for task in tasks:
+        state = task['state']
+        if state in states:
+            yield task
+
+
 def scrape():
-    family = CounterMetricFamily('koji_tasks_total', 'Help text', labels=LABELS)
-    for value, labels in koji_tasks_total():
-        family.add_metric(labels, value)
-    # Replace this in one atomic operation to avoid race condition to the collector
-    metrics['koji_tasks_total'] = family
+    tasks = retrieve_recent_koji_tasks()
+
+    koji_tasks_total_family = CounterMetricFamily(
+        'koji_tasks_total', 'Count of all koji tasks', labels=LABELS
+    )
+    for value, labels in koji_tasks_total(tasks):
+        koji_tasks_total_family.add_metric(labels, value)
+
+    koji_task_errors_total_family = CounterMetricFamily(
+        'koji_task_errors_total', 'Count of all koji task errors', labels=LABELS
+    )
+    error_tasks = only(tasks, states=error_states)
+    for value, labels in koji_tasks_total(error_tasks):
+        koji_task_errors_total_family.add_metric(labels, value)
+
+    koji_in_progress_tasks_family = GaugeMetricFamily(
+        'koji_in_progress_tasks', 'Count of all in-progress koji tasks', labels=LABELS
+    )
+    in_progress_tasks = retrieve_open_koji_tasks()
+    for value, labels in koji_tasks_total(in_progress_tasks):
+        koji_in_progress_tasks_family.add_metric(labels, value)
+
+    # Replace this in one atomic operation to avoid race condition to the Expositor
+    metrics.update({
+        'koji_tasks_total': koji_tasks_total_family,
+        'koji_task_errors_total': koji_task_errors_total_family,
+        'koji_in_progress_tasks': koji_in_progress_tasks_family,
+    })
 
 
 class Expositor(object):
